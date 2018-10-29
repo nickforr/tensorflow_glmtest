7ec06724fb6ab5ce020fed628506d55d133084cd
Nick Forrester <nickforr@me.com>
2018-10-29 20:00:22 +0000

    attempt parallel

--- vita_logreg.R
+++ vita_logreg.R
@@ -7,6 +7,7 @@ library(dplyr)
 library(purrr)
 library(tidyr)
 library(forcats)
+library(caret)
 
 vita <- readRDS("vita_mock.RDS")
 
@@ -31,9 +32,26 @@ bench::mark({
     data = cleanVita, family = binomial, weights = wts)
 }, iterations = 1)
 
+bench::mark({
+  model <- 
+    caret::train(DeadInYOE ~ I(ageNearInYOE^(-1)) + I(ageNearInYOE^(-2)) + I(ageNearInYOE^(-3)) + I(ageNearInYOE^(-4)) + YOE.C + I(ageNearInYOE^(-4)):MortGroupRGPCV18 + I(ageNearInYOE^(-4)):salBandRGPCV18, method = "glm", tuneLength = 1,
+        data = cleanVita, family = binomial, weights = wts)
+}, iterations = 1)
 
 
+library(doParallel)
+cl <- makePSOCKcluster(2)
+registerDoParallel(cl)
 
+## All subsequent models are then run in parallel
+bench::mark({
+  model <- 
+    caret::train(DeadInYOE ~ I(ageNearInYOE^(-1)) + I(ageNearInYOE^(-2)) + I(ageNearInYOE^(-3)) + I(ageNearInYOE^(-4)) + YOE.C + I(ageNearInYOE^(-4)):MortGroupRGPCV18 + I(ageNearInYOE^(-4)):salBandRGPCV18, method = "glm", tuneLength = 1,
+                 data = cleanVita, family = binomial, weights = wts)
+}, iterations = 1)
+
+## When you are done:
+stopCluster(cl)
 
 
 
